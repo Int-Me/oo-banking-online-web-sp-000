a1936d4b2019c1e900a1453d29f67841ea3e5d2d
Roland McRae <mcraeroland@gmail.com>
2020-05-13 13:20:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,9 +25,11 @@ class Transfer
 
   def reverse_transfer
     if @status = "completed"
-    sender.balance += @amount
-    receiver.balance -= @amount
-    @status = "reversed"
+      sender.balance += @amount
+      receiver.balance -= @amount
+      @status = "reversed"
+    else
+      nil
     end
   end
 end
